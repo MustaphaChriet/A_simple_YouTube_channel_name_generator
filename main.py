f7f4ac57eccefee49d8359b8f0a51a1d007cee24
Mustapha Chriet <mustaphachrietasam@gmail.com>
2024-09-20 14:27:49 +0100

    the first and the only file was added

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Welcome to the YouTube Channel Name Generator: \n")
+
+user_name = input ("What is your nickname? \n")
+
+channel_subject = input ("What is your channel about? \n")
+
+print ("You could name your channel " + "(" + channel_subject + " with " + user_name + ")")
